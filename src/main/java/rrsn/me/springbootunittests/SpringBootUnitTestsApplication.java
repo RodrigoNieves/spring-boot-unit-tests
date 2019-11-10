bffeca35084049471862cdc6de43158efbc31986
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-09 18:04:54 -0800

    Initial start.spring.io with Spring Web

---
+++ src/main/java/rrsn/me/springbootunittests/SpringBootUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package rrsn.me.springbootunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestsApplication.class, args);
+	}
+
+}
